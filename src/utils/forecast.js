c0b250957e8043ded0833d7dd45cbcb41a530b7a
Derrick Lewis <derrick.m.lewis@gmail.com>
2020-01-27 00:52:07 -0600

    Small fix.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             const summary = body.daily.data[0].summary
             const high = body.daily.data[0].temperatureHigh
             const low = body.daily.data[0].temperatureLow
-            callback(undefined, `${summary} It is currently ${temp} degrees out. There is a ${precip}% chance of rain. The high is ${high} and the low is ${low}.`)
+            callback(undefined, `${summary} It is currently ${temp} degrees out. The high is ${high} and the low is ${low}. There is a ${precip}% chance of rain.`)
         }
     })
 }
